ef832b06a3bfad1272e5d43aad961926eeafa1f7
Lucky Ozegbe <lucky.ozegbe@wsu.edu>
2026-09-20 12:04:05 -0500

    Fix Azure infrastructure policy issues

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,3 +1,89 @@
+# resource "azurerm_virtual_network" "main" {
+#   name                = "vnet-10alytics"
+#   location            = data.azurerm_resource_group.main.location
+#   resource_group_name = data.azurerm_resource_group.main.name
+#   address_space       = var.vnet_address_space
+
+#   tags = {
+#     Project = "10Alytics DevOps Assessment"
+#   }
+# }
+
+# resource "azurerm_subnet" "main" {
+#   name                 = "snet-10alytics"
+#   resource_group_name  = data.azurerm_resource_group.main.name
+#   virtual_network_name = azurerm_virtual_network.main.name
+#   address_prefixes     = var.subnet_address_prefixes
+# }
+
+# resource "azurerm_network_security_group" "main" {
+#   name                = "nsg-10alytics"
+#   location            = data.azurerm_resource_group.main.location
+#   resource_group_name = data.azurerm_resource_group.main.name
+
+#   security_rule {
+#     name                       = "Allow-HTTP"
+#     priority                   = 100
+#     direction                  = "Inbound"
+#     access                     = "Allow"
+#     protocol                   = "Tcp"
+#     source_port_range          = "*"
+#     destination_port_range     = "80"
+#     source_address_prefix      = "*"
+#     destination_address_prefix = "*"
+#   }
+
+#   security_rule {
+#     name                       = "Allow-SSH"
+#     priority                   = 110
+#     direction                  = "Inbound"
+#     access                     = "Allow"
+#     protocol                   = "Tcp"
+#     source_port_range          = "*"
+#     destination_port_range     = "22"
+#     source_address_prefix      = var.allowed_ssh_cidr
+#     destination_address_prefix = "*"
+#   }
+
+#   tags = {
+#     Project = "10Alytics DevOps Assessment"
+#   }
+# }
+
+# resource "azurerm_public_ip" "main" {
+#   name                = "pip-10alytics"
+#   location            = data.azurerm_resource_group.main.location
+#   resource_group_name = data.azurerm_resource_group.main.name
+#   allocation_method   = "Static"
+#   sku                 = "Standard"
+
+#   tags = {
+#     Project = "10Alytics DevOps Assessment"
+#   }
+# }
+
+# resource "azurerm_network_interface" "main" {
+#   name                = "nic-10alytics"
+#   location            = data.azurerm_resource_group.main.location
+#   resource_group_name = data.azurerm_resource_group.main.name
+
+#   ip_configuration {
+#     name                          = "internal"
+#     subnet_id                     = azurerm_subnet.main.id
+#     private_ip_address_allocation = "Dynamic"
+#     public_ip_address_id          = azurerm_public_ip.main.id
+#   }
+
+#   tags = {
+#     Project = "10Alytics DevOps Assessment"
+#   }
+# }
+
+# resource "azurerm_network_interface_security_group_association" "main" {
+#   network_interface_id      = azurerm_network_interface.main.id
+#   network_security_group_id = azurerm_network_security_group.main.id
+# }
+
 resource "azurerm_virtual_network" "main" {
   name                = "vnet-10alytics"
   location            = data.azurerm_resource_group.main.location
@@ -20,30 +106,6 @@ resource "azurerm_network_security_group" "main" {
   name                = "nsg-10alytics"
   location            = data.azurerm_resource_group.main.location
   resource_group_name = data.azurerm_resource_group.main.name
-
-  security_rule {
-    name                       = "Allow-HTTP"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "80"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
-    name                       = "Allow-SSH"
-    priority                   = 110
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = var.allowed_ssh_cidr
-    destination_address_prefix = "*"
-  }
 
   tags = {
     Project = "10Alytics DevOps Assessment"
